e25da46d9140d1c4648e2cf27ae238be0de20cb8
Jiří Vrba <jiri@vrba.dev>
2020-04-23 09:49:18 +0200

    Fix default route

--- src/router.js
+++ src/router.js
@@ -9,10 +9,15 @@ import TeamsView from "@/screens/TeamsView";
 
 const router = new VueRouter({
     routes: [
-        {path: '/teams', component: TeamsView},
-        {path: '/institutions', component: InstitutionsView},
-        {path: '/team/:id', component: TeamDetailsView},
+        {path: '/teams', component: TeamsView, name: 'teams'},
+        {path: '/institutions', component: InstitutionsView, name: 'institutions'},
+        {path: '/team/:id', component: TeamDetailsView, name: 'team'},
     ]
 });
+
+if (router.currentRoute.fullPath === "/")
+{
+    router.replace("/teams");
+}
 
 export default router;
