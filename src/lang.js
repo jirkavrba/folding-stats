e4405d21649589ee53f49f23a8fecff7330ac74b
Jiří Vrba <jiri@vrba.dev>
2020-05-06 23:52:14 +0200

    4e3tina 2.0 jednou bude vl8dnout cel0mu sv2tu

--- src/lang.js
+++ src/lang.js
@@ -20,6 +20,22 @@ const messages = {
         toggle_chart: "Přepnout graf (celkové skore / přírůstek za časový interval)",
         toggle_chart_increase: "Přepnout graf (denní přirůstek / 6h přírustek)"
     },
+    sc: {
+        issues: "N8vrhy a chyby",
+        teams: "Jednotliv0 t7my",
+        institutions: "Akademick0 instituce",
+        total: "4esk0 akademick0 instituce p5isp2ly celkem",
+        points: "body",
+        attribution: "Vytvo5ili studenti <a href='https://fit.cvut.cz'><b>FIT 4VUT</b></a>",
+        start_folding: "Zapojte se s n8mi i Vy!<br> <a href='https://foldingathome.org/start-folding/' target='_blank'>V9ce info zde</a>",
+        addition_request: "Pokud zde nevid9te váš t7m/insitutici, a cht2li byste ho p5idat do datab8ze, " +
+            "vytvo5te pros9m issue na <a href='https://www.github.com/jirkavrba/folding-stats/issues/new'>githubu</a>",
+        loading: "Na49t8n9...",
+        back: "Zp2t na v7pis",
+        view_at_folding: "Zobrazit na folding@home",
+        toggle_chart: "P5epnout graf (celkov0 skore / p5ír;stek za 4asov7 interval)",
+        toggle_chart_increase: "P5epnout graf (denn9 p59r;stek / 6h p59rustek)"
+    },
     en: {
         issues: "Bugs and suggestions",
         teams: "Individual teams",
